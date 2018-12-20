197f55445d8cdfc23dbb3acdcd9a2f3b766ed4db
anshumitts <anshumitts@gmail.com>
2018-12-21 02:12:09 +0530

    Added PSP and PSnDCG

--- xctools/evaluation/evaluate.py
+++ xctools/evaluation/evaluate.py
@@ -4,17 +4,24 @@ sys.path.append('../data')
 import xml_metrics
 import data_utils
 from scipy.io import loadmat
+import numpy as np
 
-def main(targets_file, predictions_file):
+def main(targets_file, train_file, predictions_file):
     # Load the dataset
-    _, labels, num_samples, _, num_labels = data_utils.read_data(targets_file)
-    true_labels = data_utils.binarize_labels(labels, num_labels)
+    _, te_labels, te_num_samples, _, te_num_labels = data_utils.read_data(targets_file)
+    true_labels = data_utils.binarize_labels(te_labels, te_num_labels)
+    
+    _, tr_labels, tr_num_samples, _, tr_num_labels = data_utils.read_data(train_file)
+    trn_labels  = data_utils.binarize_labels(tr_labels, tr_num_labels)
+
     predicted_labels = loadmat(predictions_file)['predicted_labels']
-    acc = xml_metrics.Metrices(true_labels, remove_invalid=True)
-    prec, ndcg = acc.eval(predicted_labels, 5)
-    print(xml_metrics.format(prec, ndcg))
+    inv_propen = xml_metrics.compute_inv_propesity(trn_labels, A= 0.55, B=1.5)
+    acc = xml_metrics.Metrices(true_labels, inv_propensity_scores=inv_propen, remove_invalid=False)
+    args = acc.eval(predicted_labels, 5)
+    print(xml_metrics.format(*args))
 
 if __name__ == '__main__':
-    targets_file = sys.argv[1] # Usually test data file
-    predictions_file = sys.argv[2] # In mat format
-    main(targets_file, predictions_file)
+    train_file = sys.argv[1]
+    targets_file = sys.argv[2] # Usually test data file
+    predictions_file = sys.argv[3] # In mat format
+    main(targets_file, train_file, predictions_file)
