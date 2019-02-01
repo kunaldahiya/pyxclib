5b609aa579018241c1213de4df91ee16a0fd1e20
anshumitts <anshumitts@gmail.com>
2019-02-02 02:16:35 +0530

    Using iter to read sparse data

--- processing_codes/scripts/py/nltk_tokenize.py
+++ processing_codes/scripts/py/nltk_tokenize.py
@@ -3,7 +3,7 @@ import sys
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-
+import re
 cachedStopWords = []
 cachedStopWords+=stopwords.words("english") 
 # cachedStopWords+= '\' \" : ; , < > . ~ ` ! @ # ^ & * ( ) + = - _ { } [ ] '.split(' ')
@@ -14,6 +14,23 @@ porter = nltk.PorterStemmer()
 raw_text = sys.argv[1]
 processed_text = sys.argv[2]
 to = open(processed_text,'w',encoding='latin-1')
+
+def clean_text(sentence):
+    sentence = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", sentence)
+    sentence = re.sub(r"\'s ", " ", sentence)
+    sentence = re.sub(r"\'ve ", " ", sentence)
+    sentence = re.sub(r"\'re ", " ", sentence)
+    sentence = re.sub(r"\'ll ", " ", sentence)
+    sentence = re.sub(r"\'", " \' ", sentence)
+    sentence = re.sub(r",", " , ", sentence)
+    sentence = re.sub(r"!", " ! ", sentence)
+    sentence = re.sub(r"\(", " \( ", sentence)
+    sentence = re.sub(r"\)", " \) ", sentence)
+    sentence = re.sub(r"\?", " \? ", sentence)
+    sentence = re.sub(r"\s{2,}", " ", sentence)
+    #sentence = re.sub(r'\d+', '', sentence)
+    return sentence.lower()
+
 
 def word_wise():
 	with open(raw_text,'r',encoding='latin-1') as f:
@@ -30,4 +47,10 @@ def sentence_wise():
 				paragraph.append(' '.join(words))
 			print(' . '.join(paragraph),file=to)
 
-sentence_wise()
+def sentence_wise_no_tokenise():
+	with open(raw_text,'r',encoding='latin-1') as f:
+		for sentences in f:
+			print(clean_text(sentences.strip()), file=to)
+
+
+sentence_wise_no_tokenise()
