5d5671385fc104073d47348a0b269d7a3d8c0978
anshumitts <anshumitts@gmail.com>
2019-04-24 16:54:12 +0530

    Added HIKE process code

---
+++ xclib/.random/processing_codes/STACKOVERFLOW/splitter.py
@@ -0,0 +1,84 @@
+import sys
+from functools import partial
+import numpy as np
+import pickle as p
+import scipy.sparse as sp
+import os
+from xclib.data import data_utils as du
+from bs4 import BeautifulSoup
+import re
+dataset = p.load(open(sys.argv[1], 'rb'))
+key = sys.argv[2]
+data_dir = os.path.dirname(sys.argv[1])
+lbs = dataset[key][1].tocsc()
+lbs[lbs == -1] = 1
+sort_freq = np.argsort(np.ravel(lbs.sum(axis=0)))
+max_diff = 50000
+diff = 2*max_diff
+lbs = lbs.transpose()
+num_instances = lbs.shape[1]
+num_lbs = lbs.shape[0]
+frac = 0.3
+p_diff = 10*max_diff+2
+iter_count = 0
+lbs = lbs.tolil()
+_lbs = lbs.tocsc()
+while diff > max_diff:
+    iter_count += 1
+    test_indices = {}
+    for indices in lbs[sort_freq, :].__dict__['rows']:
+        flag = np.sum(list(map(lambda x: test_indices.get(x, False), indices)))
+        if flag == 0:
+            total = max(int(frac*len(indices)), 1)
+            keys = np.random.choice(indices, size=total)
+            test_indices.update(dict((key, True) for key in keys))
+
+    test_idx = np.asarray(list(test_indices.keys()))
+    train_idx = np.setdiff1d(np.arange(num_instances), test_idx)
+    valid_lb_tst = np.where(np.ravel(_lbs[:, test_idx].sum(axis=1)) > 0)[0]
+    valid_lb_trn = np.where(np.ravel(_lbs[:, train_idx].sum(axis=1)) > 0)[0]
+    diff = num_lbs - np.intersect1d(valid_lb_tst,
+                                    valid_lb_trn, assume_unique=True).shape[0]
+    if diff < p_diff:
+        split = np.zeros((num_instances), np.int32)
+        split[test_idx] = 1
+        np.savetxt(os.path.join(data_dir, 'split.0.txt'), split, fmt="%d")
+        print("%d: diff: %d trn: %d tst: %d Labels: %d" %
+              (iter_count, diff, train_idx.shape[0], test_idx.shape[0], num_lbs))
+    if iter_count > 100:
+        break
+    print("Cout: %d, diff: %d" % (iter_count, diff), end='\r')
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+split = np.loadtxt(os.path.join(data_dir, 'split.0.txt'), dtype=np.int32)
+tst_instances = np.sum(split)
+cmp_fts = open(os.path.join(data_dir, 'corpus.txt'), 'w')
+tst_idx = 0
+trn_idx = 0
+print(len(dataset[key][0]))
+fts = cleanhtml(('My_^_$'.join(dataset[key][0])).replace('\n','')).split('My_^_$')
+print("Printing data")
+lbs = dataset[key][1].tocsr()
+fts_key = dataset[key][2]
+print(len(fts), lbs.shape)
+tst_idxs = np.where(split == 1)[0]
+trn_idxs = np.where(split == 0)[0]
+
+print(len(tst_idxs), len(trn_idxs))
+tst_lbs = lbs[tst_idxs]
+trn_lbs = lbs[trn_idxs]
+for idx, flag in enumerate(split):
+    print("%s->%s" % (fts_key[idx], fts[idx]), file=cmp_fts)
+    if idx%1000 == 1 :
+        print("[%d/%d]"%(idx, num_instances), end='\r')
+
+print("[%d/%d]"%(idx, num_instances))
+cmp_fts.close()
+du.write_sparse_file(tst_lbs, os.path.join(
+    data_dir, 'tst_lbl_mat.txt'))
+du.write_sparse_file(trn_lbs, os.path.join(
+    data_dir, 'trn_lbl_mat.txt'))
--- xclib/.random/processing_codes/scripts/py/raw_tf_idf.py
+++ xclib/.random/processing_codes/scripts/py/raw_tf_idf.py
@@ -10,6 +10,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer as tfidf
 import scipy.sparse as sp
 import pickle
 
+
 def read_split_file(split_fname):
     '''
         Return array of train/ test split
@@ -19,6 +20,7 @@ def read_split_file(split_fname):
             np.array: split
     '''
     return np.genfromtxt(split_fname, dtype=np.int)
+
 
 def _split_data(data, indices):
     """
@@ -48,33 +50,37 @@ def split_train_test(features, split):
     test_idx = np.where(split == 1)[0].tolist()
     return features[train_idx], features[test_idx]
 
+
 def main():
     data_dir = sys.argv[1]
     data = sys.argv[2]
     # text feature object
     t_obj = tfidf(min_df=3, max_df=0.8, stop_words=[], norm=None)
-    text = t_obj.fit_transform(open(data,'r',encoding='latin1'))
+    text = t_obj.fit_transform(open(data, 'r', encoding='latin1'))
     num_instances = text.shape[0]
-    data_not_present = np.where(np.ravel(text.sum(axis=1))==0)[0]
+    data_not_present = np.where(np.ravel(text.sum(axis=1)) == 0)[0]
 
-    print("DATA is not present in %d documents"%len(data_not_present))
-    
-    text = sp.hstack([text,np.zeros((num_instances,1),np.float32)]).tolil()
+    print("DATA is not present in %d documents" % len(data_not_present))
+
+    text = sp.hstack([text, np.zeros((num_instances, 1), np.float32)]).tocsr()
     vocab = t_obj.get_feature_names()+['unk']
-    np.savetxt(data_dir+'/Xf.txt',vocab,fmt="%s")
-    text[data_not_present,-1]=0
-    
-    train,test = split_train_test(text,read_split_file(sys.argv[3]))
-    flags = np.ones((num_instances,1),np.int32)
-    flags[data_not_present,0] = 0
+    np.savetxt(data_dir+'/Xf.txt', vocab, fmt="%s")
+    text[data_not_present, -1] = 0
 
-    train_dp, test_dp = split_train_test(flags,read_split_file(sys.argv[3]))
-    tr_lb = data_utils.read_sparse_file(sys.argv[4])[train_dp[:,0]!=0]
-    ts_lb = data_utils.read_sparse_file(sys.argv[5])[test_dp[:,0]!=0]
-    print(tr_lb.shape,train.shape)
-    print(ts_lb.shape,test.shape)
-    data_utils.write_data(sys.argv[6],train.tocsr()[train_dp[:,0]!=0],tr_lb)
-    data_utils.write_data(sys.argv[7],test.tocsr()[test_dp[:,0]!=0],ts_lb)
+    train, test = split_train_test(text, read_split_file(sys.argv[3]))
+    flags = np.ones((num_instances, 1), np.int32)
+    flags[data_not_present, 0] = 0
+
+    train_dp, test_dp = split_train_test(flags, read_split_file(sys.argv[3]))
+    tr_lb = data_utils.read_sparse_file(sys.argv[4])[train_dp[:, 0] != 0]
+    ts_lb = data_utils.read_sparse_file(sys.argv[5])[test_dp[:, 0] != 0]
+    print(tr_lb.shape, train.shape)
+    print(ts_lb.shape, test.shape)
+    data_utils.write_data(sys.argv[6], (train.tocsr()[train_dp[:, 0] != 0]).astype(
+        np.float32), tr_lb.astype(np.float32))
+    data_utils.write_data(sys.argv[7], (test.tocsr()[test_dp[:, 0] != 0]).astype(
+        np.float32), ts_lb.astype(np.float32))
+
 
 if __name__ == '__main__':
     main()
