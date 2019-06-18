f843a12c468210933780a6023a5c18e34dabcfc3
kunaldahiya <kd@DESKTOP-F9MA777.localdomain>
2019-06-18 20:54:15 +0530

    Deleted extra files

--- xclib/.random/processing_codes/STACKOVERFLOW/exctract_all.sh
+++
@@ -1,9 +0,0 @@
-SEVENZ="${HOME}/scratch/p7zip/bin/7z"
-WRK_DIR="${HOME}/scratch/lab/Workspace/data"
-data_directory="${WRK_DIR}/stack"
-for file in $(ls $data_directory/7zipfiles | egrep '\.7z$')
-do
-    label=$(echo $file | awk -F '.7z' '{print $1}')
-    echo $label $file
-    7z x $data_directory/7zipfiles/$file -o$data_directory/data/$label
-done
