37589922edc6bfcdc5bce9b0b967281295d5a552
Ubuntu <anshumitts@XMLC.ma3m3kbitpcebhnzpf3hh5bysa.tx.internal.cloudapp.net>
2019-06-27 12:53:20 +0000

    Cleaning repo

--- xclib/.processing_codes/Analysis/mat_to_text_convert.sh
+++
@@ -1,9 +0,0 @@
-data_dir="${1}"
-
-for file in $(ls $data_dir | egrep '\.mat$')
-do
-    label=$(echo $file | awk -F '.' '{print $1}')
-    echo $file
-    python3 scripts/mat_to_txt.py "${data_dir}/${file}" "${data_dir}/${label}.txt"
-
-done
