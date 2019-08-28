e4748446573d2a975a6781b85b2972bad00ca922
kunaldahiya <cs11b15m000001@iith.ac.in>
2019-08-29 01:35:53 +0530

    Add: compute features in sklearn framework

---
+++ xclib/examples/compute_features.py
@@ -0,0 +1,20 @@
+"""
+    Generate vectorized text and tf-idf for given text
+"""
+import os
+import sys
+import numpy as np
+from xclib.utils import text
+
+
+def compute_features():
+    corpus = [
+    'This is the first document.',
+    'This document is the second document.',
+    'And this is the third one.',
+    'Is this the first document document?']
+
+    # Compute BoW features as
+    obj = text.BoWFeatures()
+    obj.fit(corpus)
+    print(obj.transform(obj).toarray())
