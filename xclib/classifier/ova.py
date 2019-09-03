ff96b1c66042edb1152c9799437ae9773893e7cb
kunaldahiya <cs11b15m000001@iith.ac.in>
2019-09-03 19:20:54 +0530

    Train labels with zero train instance in clf

--- xclib/classifier/ova.py
+++ xclib/classifier/ova.py
@@ -3,15 +3,14 @@ from multiprocessing import Pool
 import time
 import logging
 from .base import BaseClassifier
-import scipy.sparse as sparse
+import scipy.sparse as sp
 from ._svm import train_one
-import _pickle as pickle
 from functools import partial
+from ..utils import sparse
 from ..utils import utils
-import _pickle as pickle
 import operator
-from functools import reduce
-from scipy.sparse import csr_matrix
+from ..data import data_loader
+import os
 
 
 def separate(result):
@@ -21,23 +20,67 @@ def separate(result):
 def convert_to_sparse(weight, bias):
     weight = np.vstack(weight).squeeze()
     bias = np.vstack(bias).squeeze()
-    return csr_matrix(weight), csr_matrix(bias).transpose()
+    return sp.csr_matrix(weight), sp.csr_matrix(bias).transpose()
 
 
 class OVAClassifier(BaseClassifier):
+    """
+    One-vs-all classifier for sparse or dense data
+    (suitable for large label set)
+
+    Parameters:
+    -----------
+    solver: str, optional, default='liblinear'
+        solver to use
+    loss: str, optional, default='squared_hinge'
+        loss to optimize,
+        - hinge
+        - squared_hinge
+    C: float, optional, default=1.0
+        cost in svm
+    verbose: int, optional, default=0
+        print progress in svm
+    max_iter: int, optional, default=20
+        iteration in solver
+    tol: float, optional, default=0.1
+        tolerance in solver
+    threshold: float, optional, default=0.01
+        threshold for hard thresholding (after training classifier)
+        - bias values are not touched
+        - 0.01: for sparse features
+        - 1e-5: for dense features
+    feature_type: str, optional, default='sparse'
+        feature type: sparse or dense
+    dual: boolean, optional, default=true
+        solve in primal or dual
+    use_bias: boolean, optional, default=True
+        train bias parameter or not
+    num_threads: int, optional, default=10
+        use multiple threads to parallelize
+    batch_size: int, optional, default=1000
+        train these many classifiers in parallel
+    norm: str, optional, default='l2'
+        normalize features
+    """
+
     def __init__(self, solver='liblinear', loss='squared_hinge', C=1.0,
                  verbose=0, max_iter=20, tol=0.1, threshold=0.01,
-                 use_sparse=True, dual=True, use_bias=True, num_threads=12):
-        super().__init__(verbose, use_bias, use_sparse)
+                 feature_type='sparse', dual=True, use_bias=True,
+                 num_threads=12, batch_size=1000, norm='l2'):
+        super().__init__(verbose, use_bias, feature_type)
         self.loss = loss
         self.C = C
+        self.norm = norm
         self.num_threads = num_threads
         self.verbose = verbose
         self.max_iter = max_iter
         self.threshold = threshold
         self.tol = tol
         self.dual = dual
+        self.batch_size = batch_size
         self.num_labels = None
+        self.valid_labels = None
+        self.num_labels_ = None
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger('OVAClassifier')
 
@@ -46,36 +89,86 @@ class OVAClassifier(BaseClassifier):
             Transposed classifiers before saving.
         """
         # Bias is always a dense array
-        if self.use_sparse:
-            self.weight = sparse.vstack(
+        if self.feature_type == 'sparse':
+            self.weight = sp.vstack(
                 weights, format='csr', dtype=np.float32)
-            self.bias = sparse.vstack(biases, format='csr', dtype=np.float32).toarray()
+            self.bias = sp.vstack(
+                biases, format='csr', dtype=np.float32).toarray()
         else:
             self.weight = np.vstack(weights).astype(np.float32).squeeze()
             self.bias = np.vstack(biases).astype(np.float32)
 
-    def fit(self, data, model_dir, save_after=1):
+    def get_data_loader(self, data_dir, dataset, feat_fname,
+                        label_fname, mode, batch_order):
+        """Data loader
+        - batch_order: 'label' during training
+        - batch_order: 'instances' during prediction
+        """
+        return data_loader.Dataloader(
+            batch_size=self.batch_size,
+            data_dir=data_dir,
+            dataset=dataset,
+            feat_fname=feat_fname,
+            label_fname=label_fname,
+            feature_type=self.feature_type,
+            mode=mode,
+            batch_order=batch_order,
+            norm=self.norm,
+            start_index=0,
+            end_index=-1)
+
+    def fit(self, data_dir, dataset, feat_fname, label_fname,
+            model_dir, save_after=1):
+        """Train the classifier
+        Will create batches on labels and then parallelize
+        - Not very efficient when training time per classifier is too low
+        - Will not train for labels without any datapoints
+          A list will be maintained which will used to remap labels
+          to original ids
+        Arguments:
+        ---------
+        data_dir: str
+            data directory with all files
+        dataset: str
+            Name of the dataset; like EURLex-4K
+        feat_fname: str
+            File name of training feature file
+            Should be in sparse format with header
+        label_fname: str
+            File name of training label file
+            Should be in sparse format with header
+        model_dir: str
+            dump checkpoints in this directory
+            based on save_after
+        save_after: int, default=1
+            save checkpoints after these many steps
+        """
         self.logger.info("Training!")
-        self.num_labels = data.num_labels
+        data = self.get_data_loader(
+            data_dir, dataset, feat_fname, label_fname, 'train', 'labels')
+        self.num_labels = data.num_labels  # valid labels
+        self.num_labels_ = data.num_labels_  # number of original labels
+        self.valid_labels = data.valid_labels
         weights, biases = [], []
         run_time = 0.0
-        num_batches = data._num_batches()
+        num_batches = data.num_batches
         start_time = time.time()
         for idx, batch_data in enumerate(data):
             start_time = time.time()
             batch_weight, batch_bias = self._train(
                 batch_data, self.num_threads)
             del batch_data
-            if self.use_sparse:
+            if self.feature_type == 'sparse':
                 batch_weight, batch_bias = convert_to_sparse(
                     batch_weight, batch_bias)
             batch_time = time.time() - start_time
             run_time += batch_time
             weights.append(batch_weight), biases.extend(batch_bias)
             self.logger.info(
-                "Batch: [{}/{}] completed!, time taken: {}".format(idx+1, num_batches, batch_time))
+                "Batch: [{}/{}] completed!, time taken: {}".format(
+                    idx+1, num_batches, batch_time))
             if idx != 0 and idx % save_after == 0:
-                #TODO: Delete these to save memory
+                #  TODO: Delete these to save memory?
                 self._merge_weights(weights, biases)
                 self._save_state(model_dir, idx)
                 self.logger.info("Saved state at epoch: {}".format(idx))
@@ -84,13 +177,17 @@ class OVAClassifier(BaseClassifier):
             run_time, self._compute_clf_size()))
 
     def _train(self, data, num_threads):
-        """
-            Train SVM for multiple labels
-            Args:
-                data: list: [{'X': X, 'Y': y}]
-            Returns:
-                params_: np.ndarray: (num_labels, feature_dims+1) 
-                                    +1 for bias; bias is last term
+        """Train SVM for multiple labels
+        Arguments:
+        ---------
+        data: list
+            [{'X': X, 'Y': y}]
+        Returns
+        -------
+        weights: np.ndarray
+            weight of the classifier
+        bias: float
+            bias of the classifier
         """
         with Pool(num_threads) as p:
             _func = partial(train_one, loss=self.loss,
@@ -102,24 +199,60 @@ class OVAClassifier(BaseClassifier):
         del result
         return weights, biases
 
-    def predict(self, data):
-        num_samples = data.num_samples
-        predicted_labels = sparse.lil_matrix(
-            (num_samples, data.num_valid_labels), dtype=np.float32)
+    def predict(self, data_dir, dataset, feat_fname, label_fname):
+        """Train the classifier
+        Will create batches on instance and then parallelize
+        Arguments:
+        ---------
+        data_dir: str
+            data directory with all files
+        dataset: str
+            Name of the dataset; like EURLex-4K
+        feat_fname: str
+            File name of training feature file
+            Should be in sparse format with header
+        label_fname: str
+            File name of training label file
+            Should be in sparse format with header
+            TODO: Avoid sending labels as they are not used
+        """
+        self._transpose_weights()
+        self.logger.info("Predicting!")
+        use_sparse = self.feature_type == 'sparse'
+        data = self.get_data_loader(
+            data_dir, dataset, feat_fname, label_fname, 'predict', 'instances')
+        num_instances = data.num_instances
+        predicted_labels = sp.lil_matrix(
+            (num_instances, data.num_labels), dtype=np.float32)
         start_time = time.time()
         start_idx = 0
-        for _, batch_data in enumerate(data):
+        num_batches = data.num_batches
+        for idx, batch_data in enumerate(data):
             pred = batch_data['data'][batch_data['ind']
                                       ] @ self.weight + self.bias
             utils._update_predicted(
-                start_idx, pred.view(np.ndarray) if self.use_sparse else pred,
+                start_idx, pred.view(np.ndarray) if use_sparse else pred,
                 predicted_labels)
             start_idx += pred.shape[0]
+            self.logger.info(
+                "Batch: [{}/{}] completed!".format(idx+1, num_batches))
         end_time = time.time()
         self.logger.info(
             "Prediction time/sample (ms): {}".format(
-                (end_time-start_time)*1000/num_samples))
-        return predicted_labels
+                (end_time-start_time)*1000/num_instances))
+        return self._map_to_original(predicted_labels)
+
+    def _map_to_original(self, X):
+        """Some labels were removed during training as training data was
+        not availale; remap to original mapping
+        - Assumes documents need not be remapped
+        """
+        shape = (X.shape[0], self.num_labels_)
+        return sparse._map_cols(X, self.valid_labels, shape)
+
+    def _transpose_weights(self):
+        self.weight = self.weight.transpose()
+        self.bias = self.bias.transpose()
 
     def __repr__(self):
         return "#Labels: {}, C: {}, Max_iter: {}, Threshold: {}, "\
