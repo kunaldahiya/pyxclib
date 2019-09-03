2aa19e65e9f0ca9dc6b2b51da3522fe94b2ebb60
kunaldahiya <cs11b15m000001@iith.ac.in>
2019-09-03 19:19:26 +0530

    Improved save functionality

--- xclib/classifier/base.py
+++ xclib/classifier/base.py
@@ -1,17 +1,31 @@
 import logging
 import scipy.sparse as sparse
-import _pickle as pickle
 import os
 import numpy as np
 import _pickle as pickle
 import sys
+from operator import itemgetter
 
 
 class BaseClassifier(object):
-    def __init__(self, verbose=0, use_bias=True, use_sparse=False):
+    """
+    Base classifier for sparse or dense data
+    (suitable for large label set)
+
+    Parameters:
+    -----------
+    verbose: int, optional, default=0
+        print progress in svm
+    feature_type: str, optional, default='sparse'
+        feature type: sparse or dense
+    use_bias: boolean, optional, default=True
+        train bias parameter or not
+    """
+
+    def __init__(self, verbose=0, use_bias=True, feature_type='sparse'):
         self.verbose = verbose
         self.num_labels = None
-        self.use_sparse = use_sparse
+        self.feature_type = feature_type
         logging.basicConfig(level=logging.INFO, stream=sys.stdout)
         self.logger = logging.getLogger('XC-Classifier')
         self.weight = None
@@ -19,8 +33,7 @@ class BaseClassifier(object):
 
     def _save_state(self, model_dir, epoch):
         fname = os.path.join(model_dir, 'model_state_{}.pkl'.format(epoch))
-        pickle.dump({'weight': self.weight.transpose(), 'bias': self.bias.transpose()},
-                    open(fname, 'wb'))
+        pickle.dump({self.__dict__}, open(fname, 'wb'))
 
     def _compute_clf_size(self):
         _size = 0
@@ -38,19 +51,32 @@ class BaseClassifier(object):
                 _size += self.bias.nnz*4/(1024*1024*1024)
         return _size
 
+    def state_dict(self):
+        return dict(
+            zip(self.__attrs_to_save,
+                itemgetter(*self.__attrs_to_save)(self.__dict__)))
+
+    @property
+    def __attrs_to_save(self):
+        return ['num_labels', 'valid_labels', 'num_labels_', 'weight', 'bias']
+
     def save(self, fname):
-        pickle.dump({'weight': self.weight.transpose(),
-                     'bias': self.bias.transpose(),
-                     'use_sparse': self.use_sparse,
-                     'num_labels': self.num_labels},
+        pickle.dump(self.state_dict(),
                     open(fname, 'wb'))
 
     def load(self, fname):
-        temp = pickle.load(open(fname, 'rb'))
-        self.bias = temp['bias']
-        self.weight = temp['weight']
-        self.use_sparse = temp['use_sparse']
-        self.num_labels = temp['num_labels']
+        try:
+            for key, val in pickle.load(open(fname, 'rb')).items():
+                if key in self.__attrs_to_save:
+                    self.__setattr__(key, val)
+        except FileNotFoundError:
+            print("Model not found at specified path.")
 
     def __repr__(self):
-        return "num_labels: {}, use_sparse: {}".format(self.num_labels, self.use_sparse)
+        return "num_labels: {}, feature_type: {}".format(
+            self.num_labels, self.feature_type)
+
+    def evaluate(self, true_labels, predicted_labels):
+        # TODO
+        raise NotImplementedError("Evaluate yet to be added.")
+        pass
