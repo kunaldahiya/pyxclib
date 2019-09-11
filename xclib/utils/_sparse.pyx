53ef7c3978a1d58c753e5d2511ff44630d56951e
kunaldahiya <cs11b15m000001@iith.ac.in>
2019-09-11 16:51:30 +0530

    Add: topk; Fix: _map_rows; Change: rank, binarize

--- xclib/utils/_sparse.pyx
+++ xclib/utils/_sparse.pyx
@@ -191,10 +191,26 @@ def rank_data(b):
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def rank(data, indices, indptr):
-    cdef Py_ssize_t num_rows = indptr.size
+def _rank(data, indices, indptr):
+    cdef Py_ssize_t num_rows = indptr.size - 1
     cdef Py_ssize_t idx
     cdef np.ndarray[np.int_t, ndim=1] rank = _np.empty(data.size, dtype=_np.intp)
-    for idx in range(num_rows-1):
+    for idx in range(num_rows):
         rank[indptr[idx]:indptr[idx+1]] = rank_data(-1*data[indptr[idx]:indptr[idx+1]])
     return rank
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def _topk(data, indices, indptr, k, pad_ind, pad_val):
+    cdef Py_ssize_t num_rows = indptr.size - 1
+    cdef Py_ssize_t idx, num_el, start_idx, end_idx
+    cdef np.ndarray[np.int_t, ndim=2] ind = _np.full((num_rows, k), pad_ind, _np.intp, 'C')
+    cdef np.ndarray[np.int_t, ndim=2] val = _np.full((num_rows, k), pad_val, _np.intp, 'C')
+    for idx in range(num_rows):
+        start_idx = indptr[idx]
+        end_idx = indptr[idx+1]
+        num_el = min(k, end_idx - start_idx)
+        ind[idx, :num_el] = indices[start_idx:end_idx][_np.argsort(-1*data[start_idx:end_idx])[:num_el]]
+        val[idx, :num_el] = data[start_idx:end_idx][_np.argsort(-1*data[start_idx:end_idx])[:num_el]]
+    return ind, val
